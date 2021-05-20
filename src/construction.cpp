b0407e2fb47a88655eb386476e612943c340c2f9
Shuqi Xiao <xiaosq2000@gmail.com>
2021-05-20 11:43:32 +0800

    Construction::WriteToFile()

--- src/construction.cpp
+++ src/construction.cpp
@@ -1,16 +1,18 @@
 #include "construction.h"
 
+const string kFileDirPath = "C:/toy-projects/robotics-final-project/robotics-final-project/data/rectangle-detection/";
+
 /**
  * @brief Construct a new Construction:: Construction object
  * 
  * @param bricks 
+ * @param diagonal_length_camera 
  */
 Construction::Construction(const vector<Brick> &bricks, const double &diagonal_length_camera)
 {
     this->bricks_ = bricks;
     this->diagonal_length_camera_ = diagonal_length_camera;
 }
-
 
 /**
  * @brief Find the best location to build bricks in camera frame by solving geometric median with forbidden-zone restriction
@@ -38,13 +40,13 @@ void Construction::Display(Mat &img_dst)
     cout << "the best location to build blocks(pixel): " << endl
          << this->best_location_camera_ << endl
          << "total distance(pixel): " << endl
-         << this->min_dist_camera_<< endl;
+         << this->min_dist_camera_ << endl;
 
     circle(img_dst, Point(this->best_location_camera_(0), this->best_location_camera_(1)), 7, Scalar(230, 224, 176), -1);
-    circle(img_dst, Point(this->best_location_camera_(0), this->best_location_camera_(1)), diagonal_length_camera_/2, Scalar(245, 245, 245), 1);
+    circle(img_dst, Point(this->best_location_camera_(0), this->best_location_camera_(1)), diagonal_length_camera_ / 2, Scalar(245, 245, 245), 1);
     for (size_t i = 0; i < this->bricks_.size(); i++)
     {
-        putText(img_dst, to_string(i+1), Point(this->bricks_[i].CenterCamera()(0), this->bricks_[i].CenterCamera()(1)), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 215, 255), 1, 8, false);
+        putText(img_dst, to_string(i + 1), Point(this->bricks_[i].CenterCamera()(0), this->bricks_[i].CenterCamera()(1)), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 215, 255), 1, 8, false);
         line(img_dst, Point(this->best_location_camera_(0), this->best_location_camera_(1)), Point(this->bricks_[i].CenterCamera()(0), this->bricks_[i].CenterCamera()(1)), Scalar(140, 180, 210), 1, 4, 0);
     }
     // putText(img_dst, "(" + to_string(int(this->best_location_camera_(0))) + "," + to_string(int(this->best_location_camera_(1))) + ")", Point(this->best_location_camera_(0), this->best_location_camera_(1)), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 215, 255), 1, 8, false);
@@ -55,16 +57,12 @@ void Construction::Display(Mat &img_dst)
  * 
  * @return Vector2d 
  */
-// Vector2d Construction::BestLocationCamera()
-// {
-//     return this->best_location_camera_;
-// }
 
 void Construction::Label(bool flag)
 {
     for (size_t i = 0; i < this->bricks_.size(); i++)
     {
-        this->bricks_[i].WriteDistCamera((this->bricks_[i].CenterCamera()-best_location_camera_).norm());
+        this->bricks_[i].WriteDistCamera((this->bricks_[i].CenterCamera() - best_location_camera_).norm());
     }
     if (flag == true)
     {
@@ -74,7 +72,7 @@ void Construction::Label(bool flag)
     {
         sort(this->bricks_.begin(), this->bricks_.end(), LessSort);
     }
-    
+
     for (size_t i = 0; i < this->bricks_.size(); i++)
     {
         this->bricks_[i].WriteIndex(i);
@@ -82,24 +80,45 @@ void Construction::Label(bool flag)
 }
 
 /**
- * @brief 'comp' parameter of std::sort()
+ * @brief Function pointers for std::sort() 'comp' parameter
  * 
  * @param a 
  * @param b 
  * @return true 
  * @return false 
  */
+bool GreaterSort(Brick a, Brick b) { return (a.DistCamera() > b.DistCamera()); }
+bool LessSort(Brick a, Brick b) { return (a.DistCamera() < b.DistCamera()); }
 
 /**
- * TODO x,idx in brick.h
- * TODO x,idx in brick.cpp 
- * TODO a function to calculate x in construction, .cpp
- * TODO a function to calculate x in construction, .h
+ * @brief 
  * 
+ * @return true 
+ * @return false 
  */
+void Construction::WriteToFile()
+{
+    ofstream bricks_file;
+    bricks_file.open(kFileDirPath + "bricks.txt", ios::out);
+    for (size_t i = 0; i < this->bricks_.size(); i++)
+    {
+        bricks_file << this->bricks_[i].Index()+1 << endl;
+        bricks_file << this->bricks_[i].CenterCamera() << endl;
+        bricks_file << this->bricks_[i].AngleCamera() << endl;
+    }
+    bricks_file.close();
 
-bool GreaterSort (Brick a,Brick b) { return (a.DistCamera()>b.DistCamera()); }
-bool LessSort (Brick a,Brick b) { return (a.DistCamera()<b.DistCamera()); }
+    ofstream construction_file;
+    construction_file.open(kFileDirPath + "construction.txt", ios::out);
+    construction_file << "number of bricks: " << this->bricks_.size() << endl;
+    construction_file << "average diagonal length(pixel): " << this->diagonal_length_camera_ << endl;
+    construction_file << "the best location to build blocks(pixel): " << endl
+                << this->best_location_camera_ << endl
+                << "total distance(pixel): " << endl
+                << this->min_dist_camera_ << endl;
+    construction_file.close();
+}
+
 /**
  * @brief Destroy the Construction:: Construction object
  * 

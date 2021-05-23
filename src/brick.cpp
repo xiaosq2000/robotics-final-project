d2363b2dbf3a69ee782b85c1c9ec7225dc5533a2
Shuqi Xiao <xiaosq2000@gmail.com>
2021-05-23 10:26:42 +0800

    comments

--- src/brick.cpp
+++ src/brick.cpp
@@ -45,26 +45,61 @@ double Brick::AngleCamera()
     return this->angle_camera_;
 }
 
+/**
+ * @brief Write the distance between the brick center and the construction site center in camera frame 
+ * 
+ * @param dist_camera 
+ * 
+ */
 void Brick::WriteDistCamera(double dist_camera)
 {
     this->dist_camera_ = dist_camera;
 }
 
+/**
+ * @brief Get the distance between the brick center and the construction site center in camera frame.
+ * 
+ * 
+ * @return double 
+ */
 double Brick::DistCamera()
 {
     return this->dist_camera_;
 }
 
+/**
+ * @brief Write the index of the brick, which determines the sequence to be built.
+ * 
+ * @param index 
+ * 
+ */
 void Brick::WriteIndex(int index)
 {
     this->index_ = index;
 }
 
+/**
+ * @brief Get the index of the brick.
+ * 
+ * 
+ * @return int 
+ */
 int Brick::Index()
 {
     return this->index_;
 }
 
+/**
+ * @brief Write values into `struct PosStruct pose_destination_`.
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @param yaw 
+ * @param pitch 
+ * @param roll 
+ * 
+ */
 void Brick::WritePoseDestination(double x, double y, double z, double yaw, double pitch, double roll)
 {
     this->pose_destination_.x = x;
@@ -80,6 +115,17 @@ struct PosStruct Brick::PoseDestination()
     return this->pose_destination_;
 }
 
+/**
+ * @brief Write values into `struct PosStruct pose_origin_`, which stores the origin pose of the brick in world frame.
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @param yaw 
+ * @param pitch 
+ * @param roll 
+ * 
+ */
 void Brick::WritePoseOrigin(double x, double y, double z, double yaw, double pitch, double roll)
 {
     this->pose_origin_.x = x;
@@ -90,15 +136,33 @@ void Brick::WritePoseOrigin(double x, double y, double z, double yaw, double pit
     this->pose_origin_.roll = roll;
 }
 
+/**
+ * @brief Get the origin pose of the brick in world frame.
+ * 
+ * 
+ * @return struct PosStruct 
+ */
 struct PosStruct Brick::PoseOrigin()
 {
     return this->pose_origin_;
 }
 
+/**
+ * @brief Print
+ * 
+ * 
+ */
 void Brick::Print()
 {
     cout << "center(pixel): " << endl
          << this->center_camera_ << endl
          << "angle: " << endl
-         << this->angle_camera_ << endl;
+         << this->angle_camera_ << endl
+         << "pose in world frame: " << endl
+         << this->pose_origin_.x << ", " 
+         << this->pose_origin_.y << ", " 
+         << this->pose_origin_.z << ", " 
+         << this->pose_origin_.yaw << ", " 
+         << this->pose_origin_.pitch << ", " 
+         << this->pose_origin_.roll << endl;
 }

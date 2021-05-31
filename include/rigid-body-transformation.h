bd61ea7b1fe5e7eb8414c7b3c7edcbc799b9588d
Shuqi Xiao <xiaosq2000@gmail.com>
2021-05-31 14:56:30 +0800

    HL-Robot rigid body transformation

---
+++ include/rigid-body-transformation.h
@@ -0,0 +1,53 @@
+/**
+ * @file rigid-body-transformation.h
+ * @author 肖书奇
+ * @brief HL机器人"刚体变换类"的声明
+ * @version 1.0
+ * @date 2021-05-31
+ * 
+ */
+
+#ifndef _RIGID_BODY_TRANSFORMATION_H_
+#define _RIGID_BODY_TRANSFORMATION_H_
+
+#include <iostream>
+#include "Eigen/Dense"
+#include <opencv2/core/eigen.hpp>
+
+using namespace Eigen;
+
+Matrix4d Twist2Homo(Vector3d v, Vector3d w, double theta);
+Matrix4d Rpy2Homo(double x, double y, double z, double yaw, double pitch, double roll);
+void PadenKahanSubproblem1(Vector3d p, Vector3d q, Vector3d r, Vector3d w, double &theta);
+void PadenKahanSubproblem2(Vector3d p, Vector3d q, Vector3d r, Vector3d w1, Vector3d w2, double &theta1, double &theta2);
+void PadenKahanSubproblem3(Vector3d p, Vector3d q, Vector3d r, Vector3d w, double d, double &theta);
+
+class RigidBodyTransformation
+{
+private:
+    Matrix3d cam_int_param_;
+    Matrix4d cam_ext_param_;
+
+    bool config_[3] = {1, 1, 1};
+    double link_length_1_;
+    double link_length_2_;
+    double link_length_3_;
+    double link_length_4_;
+    Vector3d w[6];
+    Vector3d q_[6];
+
+public:
+    RigidBodyTransformation(const std::string &path, const double &l1, const double &l2, const double &l3, const double &l4);
+    ~RigidBodyTransformation();
+
+    void InverseKinematics(const double *g_vec, double *joint_coor, bool *config);
+    void World2Joint(const double *rpy_coor, double *joint_coor);
+
+    void ForwardKinematics(const double *joint_coor, double *g_vec, bool *config);
+    void Joint2World(const double *joint_coor, double *rpy_coor);
+
+    void Pixel2Camera(const double *pixel_coor, double *camera_coor);
+    void Camera2Tool(const double *camera_coor, double *tool_coor);
+};
+
+#endif

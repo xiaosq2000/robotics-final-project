04f86585a574723ffaed71af2ab776bd7f4e2cd3
Shuqi Xiao <xiaosq2000@gmail.com>
2021-05-31 22:39:26 +0800

    interactive simulation

--- test/GUI-script.py
+++ test/GUI-script.py
@@ -107,7 +107,7 @@ class Application(tk.Frame):
         # action_path = r"C:/toy-projects/robotics-final-project/robotics-final-project/bin/target-recognition.exe"
         # os.startfile(action_path)
         # time.sleep(0.5)
-        self.img_dst_path = "./share/rectangle-detection/img_dst.jpg"
+        self.img_dst_path = "../share/rectangle-detection/img_dst.jpg"
         self.img_dst = ImageTk.PhotoImage(Image.open(self.img_dst_path))
         self.panel = tk.Label(self.master, image=self.img_dst)
         self.panel.pack(side="bottom", fill="both", expand="yes")
@@ -117,25 +117,25 @@ class Application(tk.Frame):
         self.canvas.get_tk_widget().destroy()
         self.toolbar.destroy()
 
-        self.data = np.loadtxt('./share/motion-plan/data_1.txt')
+        self.data = np.loadtxt('../share/motion-plan/simulation_1.txt')
 
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
         self.canvas.draw()
         self.ax = self.fig.add_subplot(111, projection="3d")
 
-        # self.x = self.data[:, 0]
-        # self.y = self.data[:, 1]
-        # self.z = self.data[:, 2]
-        # self.ax.plot(self.x, self.y, self.z)
-        # self.ax.plot(self.x, self.y, self.z)
+        self.x = self.data[:, 0]
+        self.y = self.data[:, 1]
+        self.z = self.data[:, 2]
+        self.ax.plot(self.x, self.y, self.z)
 
-        self.x = np.arange(0, 3, .01)
-        self.ax.plot(self.x, 2 * np.sin(2 * np.pi * self.x))
+        # self.x = np.arange(0, 3, .01)
+        # self.ax.plot(self.x, 2 * np.sin(2 * np.pi * self.x))
 
         self.toolbar = NavigationToolbar2Tk(self.canvas, root)
         self.toolbar.update()
         self.canvas.get_tk_widget().pack(side="top", fill="both", expand="yes")
+
 
 root = tk.Tk()
 root.title("test")

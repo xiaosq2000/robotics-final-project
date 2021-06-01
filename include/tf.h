0322f6387a6b708dfc2907809a596e1dd1da0545
Shuqi Xiao <xiaosq2000@gmail.com>
2021-06-01 21:00:11 +0800

    transformations on HL-Robot

---
+++ include/tf.h
@@ -0,0 +1,58 @@
+/**
+ * @file tf.h
+ * @author 肖书奇
+ * @brief HL机器人"坐标变换类"的声明
+ * @version 1.0
+ * @date 2021-05-31
+ * 
+ */
+
+#ifndef _RIGID_BODY_TRANSFORMATION_H_
+#define _RIGID_BODY_TRANSFORMATION_H_
+
+#include <iostream>
+#include "Eigen/Dense"
+#include <opencv2/core/eigen.hpp>
+
+using namespace Eigen;
+
+Matrix4d Twist2Homo(Vector3d v, Vector3d w, double theta);
+Matrix4d Rpy2Homo(double x, double y, double z, double yaw, double pitch, double roll);
+void PadenKahanSubproblem1(Vector3d p, Vector3d q, Vector3d r, Vector3d w, double &theta);
+void PadenKahanSubproblem2(Vector3d p, Vector3d q, Vector3d r, Vector3d w1, Vector3d w2, double &theta1, double &theta2);
+void PadenKahanSubproblem3(Vector3d p, Vector3d q, Vector3d r, Vector3d w, double d, double &theta);
+
+class TF
+{
+private:
+    Matrix3d cam_int_param_;
+    Matrix4d cam_ext_param_;
+
+    bool config_[3] = {1, 1, 1};
+    double link_length_1_;
+    double link_length_2_;
+    double link_length_3_;
+    double link_length_4_;
+    Vector3d w[6];
+    Vector3d q_[6];
+
+public:
+    TF(const std::string &cam_param_path = "../share/eye-in-hand-calibration/dst",
+                            const double &l1 = 0.491,
+                            const double &l2 = 0.45,
+                            const double &l3 = 0.45,
+                            const double &l4 = 0.084);
+    ~TF();
+
+    void InverseKinematics(const double *g_vec, double *joint_coor, bool *config);
+    void World2Joint(const double *rpy_coor, double *joint_coor);
+
+    void ForwardKinematics(const double *joint_coor, double *g_vec, bool *config);
+    void Joint2World(const double *joint_coor, double *rpy_coor);
+
+    void Pixel2Camera(const double *pixel_coor, double *camera_coor);
+    void Camera2Tool(const double *camera_coor, double *tool_coor);
+    void Tool2World(const double *rpy_tool_config, const double *tool_coor, double *rpy_coor);
+};
+
+#endif
---
+++ test/tf.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file tf.cpp
+ * @author 肖书奇
+ * @brief HL机器人"坐标变换类"的测试
+ * @version 1.0
+ * @date 2021-05-31
+ * 
+ */
+
+#include "tf.h"
+
+int main()
+{
+    // Construct a new object
+
+    // TF tf("../share/eye-in-hand-calibration/dst", 0.491, 0.45, 0.45, 0.084);
+    TF tf; // Use default parameters
+
+    // Inverse kinematics test
+
+    double rpy_coor[6]{495, -144, 530, -155, 178, -17};
+    double joint_coor[6]{0};
+    tf.World2Joint(rpy_coor, joint_coor);
+    for (size_t i = 0; i < 6; i++)
+    {
+        std::cout << joint_coor[i] << ",";
+    }
+    std::cout << std::endl;
+
+    // Forward kinematics test
+
+    // double joint_coor[6] {-16.0064, 22.887, 107.508, -1.68549, 51.1266, -56.9311};
+    // double rpy_coor[6] {0};
+    // tf.Joint2World(joint_coor, rpy_coor);
+    // for (size_t i = 0; i < 6; i++)
+    // {
+    //     std::cout << rpy_coor[i] << ",";
+    // }
+    // std::cout << std::endl;
+
+    // Pixel frame -> camera frame -> tool frame test
+
+    double pixel_coor[2]{400, 400};
+    double camera_coor[3]{0};
+    double tool_coor[3]{0};
+    tf.Pixel2Camera(pixel_coor, camera_coor);
+    for (size_t i = 0; i < 3; i++)
+    {
+        std::cout << pixel_coor[i] << ",";
+    }
+    std::cout << std::endl;
+    tf.Camera2Tool(camera_coor, tool_coor);
+    for (size_t i = 0; i < 3; i++)
+    {
+        std::cout << tool_coor[i] << ",";
+    }
+    std::cout << std::endl;
+
+    // Destory the object
+    tf.~TF();
+    return 0;
+}

1f573321035d8abdbf1542f9012e092a2013d7b9
Shuqi Xiao <xiaosq2000@gmail.com>
2021-06-01 21:05:02 +0800

    add default parameter

--- include/geometric-median.h
+++ include/geometric-median.h
@@ -2,15 +2,10 @@
 #ifndef _GEOMETRIC_MEDIAN_H_
 #define _GEOMETRIC_MEDIAN_H_
 
-#include <iostream>
-#include <vector>
-#include <Eigen/Dense>
+#include "Eigen/Dense"
 
-using namespace std;
-using namespace Eigen;
-
-double DistSum(Vector2d p, const vector<Vector2d> &points);
-bool ForbiddenZoneCheck(const Vector2d p, const vector<Vector2d> &points, const double &forbidden_zone_radius);
-void GeometricMedian(const vector<Vector2d> &points, Vector2d &center, double &min_dist, const double &lower_limit, const double &forbidden_zone_radius);
+double DistSum(Eigen::Vector2d p, const std::vector<Eigen::Vector2d> &points);
+bool ForbiddenZoneCheck(const Eigen::Vector2d p, const std::vector<Eigen::Vector2d> &points, const double &forbidden_zone_radius);
+void GeometricMedian(const std::vector<Eigen::Vector2d> &points, Eigen::Vector2d &center, double &min_dist, const double &forbidden_zone_radius, const double &lower_limit = 0.001);
 
 #endif
